2fbb5f32062db943dae34a856e8128d8d044ea5a
Rahul D'Souza <yardswp@gmail.com>
2024-03-03 11:14:40 +0000

    Added annual report that I send to Tony.

--- member_financials.py
+++ member_financials.py
@@ -1,25 +1,53 @@
 import os
-from glob import glob
+
 from dotenv import find_dotenv, load_dotenv
-from pandas import DataFrame, ExcelWriter, Timestamp, concat, read_excel
+from pandas import DataFrame, ExcelWriter, Timestamp, concat, notna, offsets
+from xlsxwriter import Workbook
+
 from utils import loadFromExcel
 
 
-load_dotenv(find_dotenv())
-
-
 def payments(file_name: str) -> DataFrame:
-    print(f'loading {file_name}')
     dir_name = os.getenv('BA_FILES_DIR', '')
     return loadFromExcel(file_name, 'Payments').set_index(['Membership ID', 'Date'])
 
 
 def write_member_financials():
-    NOW = Timestamp.today()
-    print(f'writing to Member Financials {NOW.isoformat()}')
-    with ExcelWriter('Member Financials ' + NOW.isoformat().replace(':', '-') + '.xlsx') as writer:
+    now = Timestamp.today()
+    print(f'writing to Member Financials {now.isoformat()}')
+    with ExcelWriter('Member Financials ' + now.isoformat().replace(':', '-') + '.xlsx') as writer:
         balances.to_excel(writer, sheet_name='Balances')
         payment_history.to_excel(writer, sheet_name='Payment History')
+
+
+def write_previous_month_payments():
+    last_month_end = Timestamp.today().normalize() - offsets.MonthBegin()
+    last_month_start = last_month_end - offsets.MonthBegin()
+    statement_history = loadFromExcel('Statements', 'Statements 30-91-79 27933660')
+    statement_history = statement_history[
+            (statement_history['Transaction Date'] >= last_month_start) &
+            (statement_history['Transaction Date'] < last_month_end)]\
+        .sort_index(ascending=False)
+    statement_history['Amount'] = statement_history['Credit Amount'].fillna(0) - statement_history[
+        'Debit Amount'].fillna(0)
+    statement_history = statement_history[
+            ['Transaction Date', 'Transaction Description', 'Amount']
+        ]
+    (max_row, max_col) = statement_history.shape
+    payments_filename = f'Payments {last_month_start.month_name()} {last_month_end.year}.xlsx'
+    print(f'Writing {payments_filename}')
+    with ExcelWriter(payments_filename, engine='xlsxwriter', datetime_format='d mmmm yyyy') as writer:
+        statement_history.to_excel(
+            writer, sheet_name='Payments', index=False, header=False, startrow=1)
+        payments_sheet = writer.sheets['Payments']
+        payments_sheet.add_table(1, 0, max_row, max_col - 1, {
+            'header_row': False,
+            'style': 'Table Style Medium 4',
+            'name': 'Output_for_Tony'
+        })
+        payments_sheet.set_column(0,0, 14)
+        payments_sheet.set_column(1,1, 85)
+        payments_sheet.set_column(2,2, 7)
 
 
 file_names = ['Card Issuances', 'Cheques', 'Gifts', 'PayPal', 'Statements']
@@ -35,3 +63,4 @@ balances = payment_history\
 
 if __name__ == '__main__':
     write_member_financials()
+    write_previous_month_payments()
--- utils.py
+++ utils.py
@@ -9,17 +9,26 @@ load_dotenv(find_dotenv())
 
 files_dir = os.getenv('BA_FILES_DIR', '')
 
+type MaybeDataFrame = False | pd.DataFrame
+
+
+def maybe_load_xl_dataframe(name: str, sheet: str) -> MaybeDataFrame:
+    xl_file = os.path.join(files_dir, name)
+    if os.path.exists(xl_file):
+        print(f'Loading data from {name}')
+        return pd.read_excel(xl_file, sheet)
+    return False
+
 
 def loadFromExcel(name: str, sheet: str = None) -> pd.DataFrame:
     if sheet is None:
         sheet = name
 
-    xlsx_file = os.path.join(files_dir, name + '.xlsx')
-    if os.path.exists(xlsx_file):
-        return pd.read_excel(xlsx_file, sheet)
+    df_or_false = maybe_load_xl_dataframe(name + '.xlsx', sheet)
+    if not isinstance(df_or_false, pd.DataFrame):
+        df_or_false = maybe_load_xl_dataframe(name + '.xlsm', sheet)
+        if not isinstance(df_or_false, pd.DataFrame):
+            raise FileNotFoundError(f'File {name}.xlsx or {name}.xlsm not found')
 
-    xlsm_file = os.path.join(files_dir, name + '.xlsm')
-    if os.path.exists(xlsm_file):
-        return pd.read_excel(xlsm_file, sheet)
+    return df_or_false
 
-    raise FileNotFoundError(f'File {xlsx_file} or {xlsm_file}')
